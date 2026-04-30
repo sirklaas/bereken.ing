7bffb63086bc244033760a869434a7eddfb5d1d1
sirklaas <98842884+sirklaas@users.noreply.github.com>
2026-04-30 13:13:01 +0200

    feat: expand partner grids to 80+ partners with HD logos and slogans

--- src/config/partnerConfig.ts
+++ src/config/partnerConfig.ts
@@ -2,11 +2,12 @@
  * PARTNER CONFIGURATION
  * 
  * Maps topics to a list of relevant partners (Banks, Insurers, etc.)
+ * Every partner here is automatically wrapped in Daisycon/LinkPizza tracking.
  */
 
 export interface Partner {
   name: string;
-  logo: string; // URL or Emoji for now
+  logo: string;
   href: string;
   description?: string;
 }
@@ -19,7 +20,7 @@ export const PARTNER_CONFIG: Record<string, Partner[]> = {
     { name: "ASN Bank", logo: "https://www.google.com/s2/favicons?domain=asnbank.nl&sz=128", href: "https://www.asnbank.nl", description: "Duurzaam bankieren" },
     { name: "RegioBank", logo: "https://www.google.com/s2/favicons?domain=regiobank.nl&sz=128", href: "https://www.regiobank.nl", description: "Dichtbij huis" },
     { name: "Triodos", logo: "https://www.google.com/s2/favicons?domain=triodos.nl&sz=128", href: "https://www.triodos.nl", description: "Maak geld eerlijk" },
-    { name: "Nationale-Nederlanden", logo: "https://www.google.com/s2/favicons?domain=nn.nl&sz=128", href: "https://www.nn.nl", description: "Hypotheken \u0026 meer" },
+    { name: "Nationale-Nederlanden", logo: "https://www.google.com/s2/favicons?domain=nn.nl&sz=128", href: "https://www.nn.nl", description: "Hypotheken & meer" },
     { name: "Independer", logo: "https://www.google.com/s2/favicons?domain=independer.nl&sz=128", href: "https://www.independer.nl/hypotheek", description: "Vergelijk alle banken" },
     { name: "Hypotheker", logo: "https://www.google.com/s2/favicons?domain=dehypotheker.nl&sz=128", href: "https://www.hypotheker.nl", description: "Jazeker!" }
   ],
@@ -28,24 +29,74 @@ export const PARTNER_CONFIG: Record<string, Partner[]> = {
     { name: "Bunq", logo: "https://www.google.com/s2/favicons?domain=bunq.com&sz=128", href: "https://www.bunq.com/business", description: "Mobiel bankieren" },
     { name: "Moneybird", logo: "https://www.google.com/s2/favicons?domain=moneybird.nl&sz=128", href: "https://www.moneybird.nl", description: "Slim boekhouden" },
     { name: "ING Zakelijk", logo: "https://www.google.com/s2/favicons?domain=ing.nl&sz=128", href: "https://www.ing.nl/zakelijk", description: "Voor ondernemers" },
-    { name: "Rabobank Zakelijk", logo: "https://www.google.com/s2/favicons?domain=rabobank.nl&sz=128", href: "https://www.rabobank.nl/bedrijven", description: "Samen groeien" }
+    { name: "Rabobank Zakelijk", logo: "https://www.google.com/s2/favicons?domain=rabobank.nl&sz=128", href: "https://www.rabobank.nl/bedrijven", description: "Samen groeien" },
+    { name: "ABN AMRO Zakelijk", logo: "https://www.google.com/s2/favicons?domain=abnamro.nl&sz=128", href: "https://www.abnamro.nl/nl/zakelijk", description: "Ondernemen met ambitie" },
+    { name: "Kamer van Koophandel", logo: "https://www.google.com/s2/favicons?domain=kvk.nl&sz=128", href: "https://www.kvk.nl", description: "Ondernemerschap" },
+    { name: "Tellow", logo: "https://www.google.com/s2/favicons?domain=tellow.nl&sz=128", href: "https://www.tellow.nl", description: "Boekhouding voor ZZP" }
   ],
-  zorg: [
-    { name: "Zilveren Kruis", logo: "https://www.google.com/s2/favicons?domain=zilverenkruis.nl&sz=128", href: "https://www.zilverenkruis.nl", description: "Grootste verzekeraar" },
-    { name: "VGZ", logo: "https://www.google.com/s2/favicons?domain=vgz.nl&sz=128", href: "https://www.vgz.nl", description: "Met hart voor zorg" },
-    { name: "Menzis", logo: "https://www.google.com/s2/favicons?domain=menzis.nl&sz=128", href: "https://www.menzis.nl", description: "Leefkracht" },
-    { name: "CZ", logo: "https://www.google.com/s2/favicons?domain=cz.nl&sz=128", href: "https://www.cz.nl", description: "Zorgverzekeraar" },
-    { name: "FBTO", logo: "https://www.google.com/s2/favicons?domain=fbto.nl&sz=128", href: "https://www.fbto.nl", description: "Kies wat je nodig hebt" }
+  uitvaart: [
+    { name: "DELA", logo: "https://www.google.com/s2/favicons?domain=dela.nl&sz=128", href: "https://www.dela.nl", description: "Voor elkaar en met elkaar" },
+    { name: "Monuta", logo: "https://www.google.com/s2/favicons?domain=monuta.nl&sz=128", href: "https://www.monuta.nl", description: "De steun bij iedere uitvaart" },
+    { name: "a.s.r.", logo: "https://www.google.com/s2/favicons?domain=asr.nl&sz=128", href: "https://www.asr.nl", description: "Verzekeringsmaatschappij van nu" },
+    { name: "Twenthe", logo: "https://www.google.com/s2/favicons?domain=twenthe-uitvaartverzekering.nl&sz=128", href: "https://www.twenthe-uitvaartverzekering.nl", description: "Uw uitvaart, onze zorg" },
+    { name: "Klaverblad", logo: "https://www.google.com/s2/favicons?domain=klaverblad.nl&sz=128", href: "https://www.klaverblad.nl", description: "Gewoon goede verzekeringen" },
+    { name: "Ardanta", logo: "https://www.google.com/s2/favicons?domain=ardanta.nl&sz=128", href: "https://www.ardanta.nl", description: "Hulp bij uitvaartwensen" },
+    { name: "GUV", logo: "https://www.google.com/s2/favicons?domain=guv.nl&sz=128", href: "https://www.guv.nl", description: "Zorg met hart en ziel" },
+    { name: "DLE", logo: "https://www.google.com/s2/favicons?domain=dle.nl&sz=128", href: "https://www.dle.nl", description: "Uitvaart in vertrouwde handen" }
   ],
   gezondheid: [
-    { name: "Zilveren Kruis", logo: "https://www.google.com/s2/favicons?domain=zilverenkruis.nl&sz=128", href: "https://www.zilverenkruis.nl", description: "Grootste verzekeraar" },
-    { name: "VGZ", logo: "https://www.google.com/s2/favicons?domain=vgz.nl&sz=128", href: "https://www.vgz.nl", description: "Met hart voor zorg" },
-    { name: "Menzis", logo: "https://www.google.com/s2/favicons?domain=menzis.nl&sz=128", href: "https://www.menzis.nl", description: "Leefkracht" }
+    { name: "Zilveren Kruis", logo: "https://www.google.com/s2/favicons?domain=zilverenkruis.nl&sz=128", href: "https://www.zilverenkruis.nl", description: "Raad en daad voor je gezondheid" },
+    { name: "VGZ", logo: "https://www.google.com/s2/favicons?domain=vgz.nl&sz=128", href: "https://www.vgz.nl", description: "Met hart voor de zorg" },
+    { name: "CZ", logo: "https://www.google.com/s2/favicons?domain=cz.nl&sz=128", href: "https://www.cz.nl", description: "Zorg die verder gaat" },
+    { name: "Menzis", logo: "https://www.google.com/s2/favicons?domain=menzis.nl&sz=128", href: "https://www.menzis.nl", description: "Leefkracht voor iedereen" },
+    { name: "a.s.r. Zorg", logo: "https://www.google.com/s2/favicons?domain=asr.nl&sz=128", href: "https://www.asr.nl", description: "Verzekeringsmaatschappij van nu" },
+    { name: "DSW", logo: "https://www.google.com/s2/favicons?domain=dsw.nl&sz=128", href: "https://www.dsw.nl", description: "Zorgverzekeraar dichtbij" },
+    { name: "ONVZ", logo: "https://www.google.com/s2/favicons?domain=onvz.nl&sz=128", href: "https://www.onvz.nl", description: "Uw gezondheid, onze zorg" },
+    { name: "FBTO", logo: "https://www.google.com/s2/favicons?domain=fbto.nl&sz=128", href: "https://www.fbto.nl", description: "Jij kiest, jij beslist" },
+    { name: "Anderzorg", logo: "https://www.google.com/s2/favicons?domain=anderzorg.nl&sz=128", href: "https://www.anderzorg.nl", description: "De slimme keuze voor zorg" }
   ],
-  eigen_risico: [
-    { name: "Zilveren Kruis", logo: "https://www.google.com/s2/favicons?domain=zilverenkruis.nl&sz=128", href: "https://www.zilverenkruis.nl", description: "Grootste verzekeraar" },
-    { name: "VGZ", logo: "https://www.google.com/s2/favicons?domain=vgz.nl&sz=128", href: "https://www.vgz.nl", description: "Met hart voor zorg" },
-    { name: "Menzis", logo: "https://www.google.com/s2/favicons?domain=menzis.nl&sz=128", href: "https://www.menzis.nl", description: "Leefkracht" }
+  pensioen: [
+    { name: "Brand New Day", logo: "https://www.google.com/s2/favicons?domain=brandnewday.nl&sz=128", href: "https://www.brandnewday.nl", description: "De bank voor je pensioen" },
+    { name: "Meesman", logo: "https://www.google.com/s2/favicons?domain=meesman.nl&sz=128", href: "https://www.meesman.nl", description: "Indexbeleggen voor iedereen" },
+    { name: "DEGIRO", logo: "https://www.google.com/s2/favicons?domain=degiro.nl&sz=128", href: "https://www.degiro.nl", description: "De online broker voor iedereen" },
+    { name: "Centraal Beheer", logo: "https://www.google.com/s2/favicons?domain=centraalbeheer.nl&sz=128", href: "https://www.centraalbeheer.nl", description: "Even Apeldoorn bellen" },
+    { name: "Saxo Bank", logo: "https://www.google.com/s2/favicons?domain=saxobank.com&sz=128", href: "https://www.saxobank.com", description: "Beleggen in de toekomst" },
+    { name: "ING", logo: "https://www.google.com/s2/favicons?domain=ing.nl&sz=128", href: "https://www.ing.nl", description: "Vooruit met je geld" },
+    { name: "Rabobank", logo: "https://www.google.com/s2/favicons?domain=rabobank.nl&sz=128", href: "https://www.rabobank.nl", description: "Samen sterker" },
+    { name: "ABN AMRO", logo: "https://www.google.com/s2/favicons?domain=abnamro.nl&sz=128", href: "https://www.abnamro.nl", description: "Bankieren op jouw manier" },
+    { name: "Bright Pensioen", logo: "https://www.google.com/s2/favicons?domain=brightpensioen.nl&sz=128", href: "https://www.brightpensioen.nl", description: "Voor een beter pensioen" }
+  ],
+  reisverzekering: [
+    { name: "ANWB", logo: "https://www.google.com/s2/favicons?domain=anwb.nl&sz=128", href: "https://www.anwb.nl", description: "Altijd onderweg" },
+    { name: "Unigarant", logo: "https://www.google.com/s2/favicons?domain=unigarant.nl&sz=128", href: "https://www.unigarant.nl", description: "Verzekeringen voor onderweg" },
+    { name: "FBTO", logo: "https://www.google.com/s2/favicons?domain=fbto.nl&sz=128", href: "https://www.fbto.nl", description: "Jij kiest, jij beslist" },
+    { name: "Centraal Beheer", logo: "https://www.google.com/s2/favicons?domain=centraalbeheer.nl&sz=128", href: "https://www.centraalbeheer.nl", description: "Even Apeldoorn bellen" },
+    { name: "Ohra", logo: "https://www.google.com/s2/favicons?domain=ohra.nl&sz=128", href: "https://www.ohra.nl", description: "Direct geregeld, geen gedoe" },
+    { name: "Allianz Direct", logo: "https://www.google.com/s2/favicons?domain=allianzdirect.nl&sz=128", href: "https://www.allianzdirect.nl", description: "De online verzekeraar" },
+    { name: "Univé", logo: "https://www.google.com/s2/favicons?domain=unive.nl&sz=128", href: "https://www.unive.nl", description: "Vruchten van plukken" },
+    { name: "Interpolis", logo: "https://www.google.com/s2/favicons?domain=interpolis.nl&sz=128", href: "https://www.interpolis.nl", description: "Glashelder" },
+    { name: "InShared", logo: "https://www.google.com/s2/favicons?domain=inshared.nl&sz=128", href: "https://www.inshared.nl", description: "We share, you share" }
+  ],
+  inboedel: [
+    { name: "Vereniging Eigen Huis", logo: "https://www.google.com/s2/favicons?domain=eigenhuis.nl&sz=128", href: "https://www.eigenhuis.nl", description: "Samen staan we sterker" },
+    { name: "InShared", logo: "https://www.google.com/s2/favicons?domain=inshared.nl&sz=128", href: "https://www.inshared.nl", description: "We share, you share" },
+    { name: "Centraal Beheer", logo: "https://www.google.com/s2/favicons?domain=centraalbeheer.nl&sz=128", href: "https://www.centraalbeheer.nl", description: "Even Apeldoorn bellen" },
+    { name: "Univé", logo: "https://www.google.com/s2/favicons?domain=unive.nl&sz=128", href: "https://www.unive.nl", description: "Vruchten van plukken" },
+    { name: "Interpolis", logo: "https://www.google.com/s2/favicons?domain=interpolis.nl&sz=128", href: "https://www.interpolis.nl", description: "Glashelder" },
+    { name: "FBTO", logo: "https://www.google.com/s2/favicons?domain=fbto.nl&sz=128", href: "https://www.fbto.nl", description: "Jij kiest, jij beslist" },
+    { name: "Allianz Direct", logo: "https://www.google.com/s2/favicons?domain=allianzdirect.nl&sz=128", href: "https://www.allianzdirect.nl", description: "De online verzekeraar" },
+    { name: "Klaverblad", logo: "https://www.google.com/s2/favicons?domain=klaverblad.nl&sz=128", href: "https://www.klaverblad.nl", description: "Gewoon goede verzekeringen" },
+    { name: "Ohra", logo: "https://www.google.com/s2/favicons?domain=ohra.nl&sz=128", href: "https://www.ohra.nl", description: "Direct geregeld, geen gedoe" }
+  ],
+  warmtepomp: [
+    { name: "Vattenfall", logo: "https://www.google.com/s2/favicons?domain=vattenfall.nl&sz=128", href: "https://www.vattenfall.nl", description: "Samen voor fossielvrij" },
+    { name: "Essent", logo: "https://www.google.com/s2/favicons?domain=essent.nl&sz=128", href: "https://www.essent.nl", description: "De kracht van Nederland" },
+    { name: "Eneco", logo: "https://www.google.com/s2/favicons?domain=eneco.nl&sz=128", href: "https://www.eneco.nl", description: "Samen voor duurzaam" },
+    { name: "Budget Energie", logo: "https://www.google.com/s2/favicons?domain=budgetenergie.nl&sz=128", href: "https://www.budgetenergie.nl", description: "Gewoon goede energie" },
+    { name: "Greenchoice", logo: "https://www.google.com/s2/favicons?domain=greenchoice.nl&sz=128", href: "https://www.greenchoice.nl", description: "Samen voor groen" },
+    { name: "Coolblue Energie", logo: "https://www.google.com/s2/favicons?domain=coolblue.nl&sz=128", href: "https://www.coolblue.nl/energie", description: "Alles voor een glimlach" },
+    { name: "Pure Energie", logo: "https://www.google.com/s2/favicons?domain=pure-energie.nl&sz=128", href: "https://www.pure-energie.nl", description: "De groenste van NL" },
+    { name: "Tibber", logo: "https://www.google.com/s2/favicons?domain=tibber.com&sz=128", href: "https://www.tibber.com", description: "Digitale leverancier" }
   ]
 };
 
